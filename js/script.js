0a50396ab24b5b1d6d1650c6462931935dd061ad
WycliffDembe <dembecliff@gmail.com>
2021-12-22 14:13:45 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function change_div_color(){
+  document.getElementById("mainDiv").style.backgroundColor = "lightblue";
+}
+
+function change_color(id){
+  document.getElementById(String(id)).style.backgroundColor = "yellow";
+}
